6f9b4d6e7080ddd93d806bbd038cac6c9d05b6a6
“CodingMrWang” <zexianw@andrew.cmu.edu>
2018-01-21 01:22:26 +1030

    final

---
+++ MyGit.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.text.SimpleDateFormat;
+class MyGit {
+    public static void myGit() {
+        Date date = new Date();
+        System.out.println("Date: " + date);
+        System.out.println("Zexian Wang");
+        System.out.println("I love Cloud Computing");
+    }
+    public static void main(String[] args) {
+        myGit();
+    }
+}
