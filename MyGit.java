db1580961e8f0a9948cdfc38d8a27de1a644336f
“CodingMrWang” <zexianw@andrew.cmu.edu>
2018-01-21 01:02:18 +1030

    my first try

---
+++ MyGit.java
@@ -0,0 +1,9 @@
+class MyGit {
+    public static void myGit() {
+        System.out.println("Zexian Wang");
+        System.out.println("I love Cloud Computing");
+    }
+    public static void main(String[] args) {
+        myGit();
+    }
+}
